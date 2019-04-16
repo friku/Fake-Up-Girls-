910e749ac0d56eadfa655a15bc674dc570a95319
friku <riku.fujimoto@gmail.com>
2019-04-16 22:20:39 +0900

    self0_batch64_lrd2^-4_lrg2^-4_ch64_long

--- models_64x64_pos.py
+++ models_64x64_pos.py
@@ -301,10 +301,12 @@ def generator_self(z, dim=64, reuse=True, training=True):
         y = fc_bn_relu(z, 4 * 4 * dim * 8)
         y = tf.reshape(y, [-1, 4, 4, dim * 8])
         y = dconv_bn_relu(y, dim * 4, 3, 2)
+        # y = self_attention_2(y, dim * 4, scope='self_attention3')
         y = dconv_bn_relu(y, dim * 2, 3, 2)
+        # y = self_attention_2(y, dim * 2, scope='self_attention2')
         y = dconv_bn_relu(y, dim * 1, 3, 2)
         y = self_attention_2(y, dim * 1, scope='self_attention1')
-        img = tf.tanh(dconv(y, 3, 3, 2))
+        img = tf.tanh(dconv(y, 3, 5, 2))
         return img
 
 
@@ -312,10 +314,11 @@ def discriminator_wgan_gp_self(img, dim=64, reuse=True, training=True):
     with tf.variable_scope('discriminator', reuse=reuse):
         conv_ln_lrelu = partial(conv, normalizer_fn=ln, activation_fn=lrelu, biases_initializer=None)
         # y = self_attention_2(y, dim * 1, scope='self_attention1')
-        y = lrelu(conv(img, dim, 3, 2))
+        y = lrelu(conv(img, dim, 3, 1))
         y = self_attention_2(y, dim * 1, scope='self_attention1')
         y = conv_ln_lrelu(y, dim * 1, 3, 2)
         y = conv_ln_lrelu(y, dim * 2, 3, 2)
         y = conv_ln_lrelu(y, dim * 4, 3, 2)
+        y = conv_ln_lrelu(y, dim * 8, 3, 2)
         logit = fc(y, 1)
         return logit
--- train_tiktok_biggan.py
+++ train_tiktok_biggan.py
@@ -98,7 +98,7 @@ it_cnt, update_cnt = utils.counter()
 saver = tf.train.Saver(max_to_keep=5)
 # summary writer
 
-dir_name = "tik_"+str(imgsize)+"_self0_batch64_lrd2^-4_lrg2^-4_ch64_long_light"
+dir_name = "tik_"+str(imgsize)+"_self0_batch64_lrd2^-4_lrg2^-4_ch64_long"
 
 summary_writer = tf.summary.FileWriter('./summaries/' + dir_name, sess.graph)
 
